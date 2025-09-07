a399472cc486d2f7654faac84e3472dbb6a7edde
Micheal-Eft <evtushok.m.m@mail.ru>
2025-09-07 17:02:37 +0300

    Add files via upload

--- Home_work_14/js/main.js
+++ Home_work_14/js/main.js
@@ -12,6 +12,8 @@
         document.body.classList.toggle('body--opend-menu')
     }
 
+    // =====
+
     const modal = document.querySelector('.modal')
     const modalButton = document.querySelector('.about__img-button')
 
@@ -32,4 +34,64 @@
             document.body.classList.remove('body--modal-opened')
         }
     }
+
+    // =====
+
+    const tabControls = document.querySelector('.tab-controls')
+    tabControls.addEventListener('click', toggleTab)
+    function toggleTab(e) {
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        e.preventDefault()
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const showContent = document.querySelector('.tab-content--show')
+
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--active')
+        }
+        if (showContent) {
+            showContent.classList.remove('tab-content--show')
+        }
+
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
+    }
+
+    // =====
+
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+    accordionLists.forEach(el => {
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+            const accordionControl = e.target.closest('.accordion-list__control')
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement
+            const accordionContent = accordionControl.nextElementSibling
+
+            if (accordionOpenedItem && accordionOpenedItem != accordionItem){
+                accordionOpenedItem.classList.remove('accordion-list__item--opened')
+                accordionOpenedContent.style.maxHeight = null
+            }
+            console.log(1)
+            accordionItem.classList.toggle('accordion-list__item--opened')
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+            }
+            else {
+                accordionContent.style.maxHeight = null
+            }
+        })
+    })
 })()
