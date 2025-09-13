cffcff693ff998023aa224793007dfb24da4e3dd
Micheal-Eft <evtushok.m.m@mail.ru>
2025-09-13 14:27:05 +0300

    Add files via upload

--- Home_work_14/js/main.js
+++ Home_work_14/js/main.js
@@ -155,4 +155,12 @@
             },
         }
     });
+
+
+    // =====
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
